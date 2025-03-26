be043808c164ed3722467b77f08fd6cb551351a7
ETGLLND <et.galland@gmail.com>
2025-03-26 22:20:30 +0100

    Create hero section v4

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,17 @@
-console.log("Coucou les amis gentisl");
+const root = document.querySelector(".reviews-bloc > .reviews-container");
+const prevBtn = document.querySelector(".controls > .left");
+const nextBtn = document.querySelector(".controls > .right");
+
+const flkty = new Flickity(root, {
+  prevNextButtons: false,
+  pageDots: false,
+  gap,
+});
+prevBtn.addEventListener("click", () => {
+  console.log("previous");
+  flkty.previous();
+});
+nextBtn.addEventListener("click", () => {
+  console.log("next");
+  flkty.next();
+});
